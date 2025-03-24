f389487818bfcc526a7ef355d11c70deb299fb8c
TheBlueBarron <arcadiacomfort@gmail.com>
2025-03-23 19:02:22 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ComingSoon from "./pages/ComingSoon";
 
+
 export default function App() {
   return (
     <Router>
